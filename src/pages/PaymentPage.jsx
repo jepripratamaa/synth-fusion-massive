acaceac8115e1a19a27aa2cd050079784ece17a5
jepripratamaa <jepripratama25@gmail.com>
2023-12-05 02:19:16 +0700

    membuat page payment

---
+++ src/pages/PaymentPage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import { payment } from '../data';
+
+const PaymentPage = () => {
+    return (
+        <div className='blog w-100 payment-bg'>
+        <Container>
+            <Row>
+                <Col>
+                    <h1 className='text-white text-center fw-bold'>Payment Method</h1>
+                </Col>
+                <div className='blog-pos'>
+                    {payment.map((payment) => {
+                        return(
+                            <div>
+                                <Col key={payment.id} className=''>
+                                    <div className='payment'>
+                                        <img src={payment.image} alt="" />
+                                    </div>
+                                </Col>
+                            </div>
+                        )
+                    })}
+                </div>
+            </Row>
+        </Container>
+        </div>
+    );
+}
+
+export default PaymentPage
