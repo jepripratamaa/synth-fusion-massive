b8bf69fae4de359cf2d2dfbf08096f6d0a8a0631
jepripratamaa <jepripratama25@gmail.com>
2023-12-05 02:18:50 +0700

    membuat page community page

---
+++ src/pages/CommunityPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CommunityPage = () => {
+    return (
+        <div className='page-community'>
+            <div className='container py-5'>
+                <div>
+                    <h1>SATURDAY NIGHT PARTY</h1>
+                    <h5>Once a Month on Saturday Night </h5>
+                </div>
+                <div>
+                    <p>
+                    Every month, the vibrant and eagerly anticipated Saturday Night Party comes alive, creating an atmosphere of excitement and camaraderie that transcends the boundaries of membership. This monthly extravaganza is a celebration open to both members and non-members alike, fostering a sense of inclusivity and unity within the community.
+                    <br /> <br />
+                    Hosted exclusively online in the community's dynamic Discord channel, the Saturday Night Party unfolds over a delightful two-hour span, providing ample time for participants to engage in lively conversations, share laughter, and forge new connections. This digital gathering space becomes a virtual haven where people from diverse backgrounds converge to revel in the shared joy of the moment.
+                    <br /> <br />
+                    The Saturday Night Party is not just an event; it's a testament to the power of community and the spirit of togetherness. Whether you're a longstanding member or a newcomer eager to experience the vibrant energy, this monthly soirée promises an unforgettable evening filled with entertainment, laughter, and the kind of connections that make a community truly special.
+                    <br /> <br />
+                    As the clock ticks away on the designated Saturday night each month, the anticipation builds, setting the stage for an immersive experience that transcends the limitations of physical spaces. In the realm of the community's online Discord channel, the Saturday Night Party becomes a melting pot of personalities, fostering an environment where friendships flourish, and the community spirit thrives.
+                    <br /> <br />
+                    So, mark your calendars and join us as we come together for the Saturday Night Party—a monthly rendezvous that transforms ordinary Saturday nights into extraordinary moments of shared joy and unforgettable memories within the vibrant tapestry of our online community. Join the discord server here : 
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CommunityPage
